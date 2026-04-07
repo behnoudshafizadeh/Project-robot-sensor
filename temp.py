0608f81b6e69bcd7f2257950c2d9f8fa01fe5259
Behnoud.SH <behnud.shafizadeh@gmail.com>
2026-04-07 13:45:39 +0200

    Initial commit - Robot obstacle avoidance simulation

---
+++ temp.py
@@ -0,0 +1,37 @@
+from ROBOT import Graphics,Robot,Ultrasonic
+import math
+import pygame
+
+MAP_DIMENSIONS=(680,1200)
+
+gfx = Graphics(MAP_DIMENSIONS,'DDR.png','ObstacleMap.png')
+
+start=(200,200)
+robot=Robot(start,0.01*3779.52)
+
+sensor_range=250,math.radians(40)
+ultra_sonic=Ultrasonic(sensor_range,gfx.map)
+
+dt=0
+last_time= pygame.time.get_ticks()
+
+running= True
+
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+       running = False
+
+
+  dt=(pygame.time.get_ticks()-last_time)/1000
+  last_time=pygame.time.get_ticks()
+
+  gfx.map.blit(gfx.map_img,(0,0))
+
+  robot.kinematics(dt)
+  gfx.draw_robot(robot.x,robot.y,robot.heading)
+  point_cloud=ultra_sonic.sense_obstacles(robot.x,robot.y,robot.heading)
+  robot.avoid_obstacles(point_cloud,dt)
+  gfx.draw_sensor_data(point_cloud)
+
+  pygame.display.update()
